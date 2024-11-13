7c52b4dfcb4039e7bbe8fee828ce45b19428cee2
Wambui Ndungu <wambuin18@gmail.com>
2024-11-13 10:45:19 +0300

    Project Table

--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import projects from "@/data/projects.json"
 
 const Projects = () => {
-  return (
-    <div>Projects</div>
-  )
-}
 
-export default Projects
+  return (
+    <div>
+      <table className="w-full border-collapse">
+        <thead className="border-b border-black">
+          <tr className="">
+            <th>Project</th>
+            <th>Category</th>
+            <th>Industry</th>
+          </tr>
+        </thead>
+        <tbody className="">{projects.map((project) => (
+          <tr className="border-b border-black" key={project.id}>
+            <td>{project.title}</td>
+            <td>{project.category}</td>
+            <td>{project.industy}</td>
+            </tr>
+        ))}</tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Projects;
